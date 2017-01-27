cf2f1668789cff2ec5334c68ebb5444eb5fc8fa5
sameclone <same_clone@mail.ru>
2017-01-27 09:35:15 +0600

    Examples from chapter 6 OCP study guide

--- src/OCJP8/chap5/localization/TestsFormatDateTime.java
+++ src/OCJP8/chap5/localization/TestsFormatDateTime.java
@@ -22,10 +22,10 @@ public class TestsFormatDateTime {
 
         LocalDate date1 = LocalDate.of(2020, Month.JANUARY, 20);
         LocalTime time1 = LocalTime.of(11, 12, 34);
-        LocalDateTime dateTime = LocalDateTime.of(date1,time1);
-        System.out.println("ISO_LOCAL_DATE: "+date1.format(DateTimeFormatter.ISO_LOCAL_DATE));
-        System.out.println("ISO_LOCAL_TIME: "+time1.format(DateTimeFormatter.ISO_LOCAL_TIME));
-        System.out.println("ISO_LOCAL_DATE_TIME: "+dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        LocalDateTime dateTime = LocalDateTime.of(date1, time1);
+        System.out.println("ISO_LOCAL_DATE: " + date1.format(DateTimeFormatter.ISO_LOCAL_DATE));
+        System.out.println("ISO_LOCAL_TIME: " + time1.format(DateTimeFormatter.ISO_LOCAL_TIME));
+        System.out.println("ISO_LOCAL_DATE_TIME: " + dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
 
         System.out.println("--------------------------");
 
@@ -44,6 +44,24 @@ public class TestsFormatDateTime {
         //time cannot be formatted as date
         //System.out.println(time1.format(shortDateTime1));//java.time.temporal.UnsupportedTemporalTypeException: Unsupported field: DayOfMonth
 
+        System.out.println("--------------------------");
+        LocalDate date2 = LocalDate.of(2020, Month.JANUARY, 20);
+        LocalTime time2 = LocalTime.of(11, 12, 34);
+        LocalDateTime dateTime2 = LocalDateTime.of(date2, time2);
+
+        DateTimeFormatter shortF = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+        DateTimeFormatter mediumF = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+
+        System.out.println("SHORT: "+shortF.format(dateTime2));
+        System.out.println("MEDIUM: "+mediumF.format(dateTime2));
+
+        System.out.println("--------------------------");
+
+        DateTimeFormatter f  = DateTimeFormatter.ofPattern("MM dd yyyy");
+        LocalDate date3 = LocalDate.parse("01 02 2015", f);
+        System.out.println(date3);
+        LocalTime time3 = LocalTime.parse("11:22");
+        System.out.println(time3);
 
 
 
---
+++ src/OCJP8/chap6/TestsMultiCatch.java
@@ -0,0 +1,62 @@
+package OCJP8.chap6;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+/**
+ * Created by yevgeniya.zuyeva on 27.01.2017.
+ */
+public class TestsMultiCatch {
+    public static void main(String[] ars) {
+
+        System.out.println("---------------------");
+        try {
+            Path path = Paths.get("dolphinsBorn.txt");
+            String text = new String(Files.readAllBytes(path));
+            LocalDate date = LocalDate.parse(text);
+            System.out.println(date);
+        } catch (DateTimeParseException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+
+        System.out.println("--------BAD----------");
+        try {
+            Path path = Paths.get("dolphinsBorn.txt");
+            String text = new String(Files.readAllBytes(path));
+            LocalDate date = LocalDate.parse(text);
+            System.out.println(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("--------helper method-------------");
+
+        try {
+            Path path = Paths.get("dolphinsBorn.txt");
+            String text = new String(Files.readAllBytes(path));
+            LocalDate date = LocalDate.parse(text);
+            System.out.println(date);
+        } catch (DateTimeParseException e) {
+            handleException(e);
+        } catch (IOException e) {
+            handleException(e);
+        }
+
+
+    }
+
+    private static void handleException(Exception e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
+    }
+}
